1efa596a6247988ce263a82ea5ee489e13debf2d
MikleBr <barulin.ma@gmail.com>
2022-02-22 00:15:45 +0300

    Добавил библиотеку react-code-input. Настроил ее под проект

--- src/components/UI/CodeInput/CodeInput.tsx
+++ src/components/UI/CodeInput/CodeInput.tsx
@@ -1,14 +1,37 @@
 import React, { CSSProperties } from 'react';
+import ReactCodeInput, { InputModeTypes } from 'react-code-input';
+
+import s from './CodeInput.module.scss';
 
 type Props = {
+  name: string;
   sx?: CSSProperties;
   value?: string;
   fieldsCount?: number;
+  type?: 'number' | 'text' | 'password' | 'tel' | undefined;
+  inputMode?: InputModeTypes;
   onChange: (value: string) => void;
 };
 
 export function CodeInput({
-  sx, value, onChange, fieldsCount = 4,
+  name,
+  sx,
+  value,
+  onChange,
+  fieldsCount = 4,
+  type = 'text',
+  inputMode = 'numeric',
 }: Props) {
-  return <div>CodeInput</div>;
+  return (
+    <div className={s.codeInput} style={sx}>
+      <ReactCodeInput
+        name={name}
+        value={value}
+        onChange={onChange}
+        type={type}
+        fields={fieldsCount}
+        inputMode={inputMode}
+      />
+    </div>
+  );
 }
