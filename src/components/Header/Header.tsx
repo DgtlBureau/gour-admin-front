0b0eb0e5cd08e28efabef15ba53d5544ae1990d3
artyom vorokhta <a.vorohta@newshift.ru>
2022-02-21 16:21:41 +0300

    header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,7 @@
-import React, { ElementType, ReactNode } from 'react';
-import MUIAppBar from '@mui/material/AppBar';
+import React, { ReactNode } from 'react';
+import { Toolbar, Typography, AppBar as MUIAppBar } from '@mui/material';
+
+import s from './Header.module.scss';
 
 type Props = {
   leftTitle: string;
@@ -7,5 +9,19 @@ type Props = {
 };
 
 export function Header({ leftTitle, rightContent }: Props) {
-  return <>Header</>;
+  return (
+    <MUIAppBar className={s.bar} position="static">
+      <Toolbar className={s.wrapper}>
+        <Typography
+          variant="h5"
+          color="black"
+          component="div"
+          sx={{ flexGrow: 1 }}
+        >
+          {leftTitle}
+        </Typography>
+        {rightContent}
+      </Toolbar>
+    </MUIAppBar>
+  );
 }
