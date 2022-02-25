e18a51d43145cb335c66888d2f541b9816c126ee
MikleBr <barulin.ma@gmail.com>
2022-02-25 11:46:32 +0300

    Добавил кнопку

--- src/components/UI/CommentTextarea/CommentTextarea.tsx
+++ src/components/UI/CommentTextarea/CommentTextarea.tsx
@@ -1,15 +1,20 @@
 import React, { ChangeEventHandler, CSSProperties, FocusEventHandler } from 'react';
+import { Button } from '../Button/Button';
 import { Textarea } from '../Textarea/Textarea';
+
+import s from './CommentTextarea.module.scss';
 
 type Props = {
   minRows?: number;
   maxRows?: number;
   placeholder?: string;
+  buttonVariant?: 'text' | 'outlined' | 'contained';
   style?: CSSProperties;
   defaultValue?: string;
   onChange: ChangeEventHandler<HTMLTextAreaElement>;
   onBlur?: ChangeEventHandler<HTMLTextAreaElement>;
   onFocus?: FocusEventHandler<HTMLTextAreaElement>;
+  onClick?(): void;
 };
 
 export function CommentTextarea({
@@ -18,20 +23,27 @@ export function CommentTextarea({
   placeholder,
   style,
   defaultValue,
+  buttonVariant,
+  onClick,
   onChange,
   onBlur,
   onFocus,
 }: Props) {
   return (
-    <Textarea
-      onChange={onChange}
-      onBlur={onBlur}
-      onFocus={onFocus}
-      minRows={minRows}
-      maxRows={maxRows}
-      defaultValue={defaultValue}
-      placeholder={placeholder}
-      sx={style}
-    />
+    <div className={s.commentTextarea}>
+      <Textarea
+        onChange={onChange}
+        onBlur={onBlur}
+        onFocus={onFocus}
+        minRows={minRows}
+        maxRows={maxRows}
+        defaultValue={defaultValue}
+        placeholder={placeholder}
+        sx={style}
+      />
+      <Button onCLick={onClick} variant={buttonVariant}>
+        Отправить
+      </Button>
+    </div>
   );
 }
