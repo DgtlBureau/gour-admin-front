6358c4ca84cacd87944940252e1bb6501a861e58
artyom vorokhta <a.vorohta@newshift.ru>
2022-03-02 16:52:38 +0300

    comment

--- src/components/UI/Comment/Comment.tsx
+++ src/components/UI/Comment/Comment.tsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import { Card, CardContent } from '@mui/material';
+
+import blueStarIcon from '../../../assets/icons/comment/stars/blue-star.svg';
+import grayStarIcon from '../../../assets/icons/comment/stars/gray-star.svg';
+
 import s from './Comment.module.scss';
 
 export type CommentProps = {
@@ -8,6 +13,32 @@ export type CommentProps = {
     text: string;
 };
 
-export function Comment(props: CommentProps) {
-  return <div>Comment</div>;
+export function Comment({
+  title,
+  grade,
+  date,
+  text,
+}: CommentProps) {
+  return (
+    <Card className={s.card}>
+      <CardContent className={s.content}>
+        <span className={s.title}>{title}</span>
+
+        <div className={s.grade_n_date}>
+          {
+            Array.from({ length: 5 }, (_, i) => (
+              <img
+                src={i < grade ? blueStarIcon : grayStarIcon}
+                className={s.star}
+                alt=""
+              />
+            ))
+          }
+          <span className={s.date}>{date}</span>
+        </div>
+
+        <span className={s.text}>{text}</span>
+      </CardContent>
+    </Card>
+  );
 }
