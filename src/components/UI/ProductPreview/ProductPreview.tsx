313ad030fd9721cd56ebf5ce0d6821350b32eba3
artyom vorokhta <a.vorohta@newshift.ru>
2022-03-14 14:43:16 +0300

    product preview

---
+++ src/components/UI/ProductPreview/ProductPreview.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Grid } from '@mui/material';
+
+import s from './ProductPreview.module.scss';
+
+export type ProductPreviewProps = {
+  title: string;
+  category: string;
+  price: number;
+  imageSrc: string;
+  onDelete: () => void;
+  onEdit: () => void;
+}
+
+export function ProductPreview({
+  title,
+  category,
+  price,
+  imageSrc,
+  onDelete,
+  onEdit,
+}: ProductPreviewProps) {
+  return (
+    <Grid className={s.preview} container>
+      <Grid item xs>
+        <img src={imageSrc} alt="" />
+      </Grid>
+      <Grid className={s.text} item xs>
+        <span>{title}</span>
+      </Grid>
+      <Grid className={s.text} item xs>
+        <span>{category}</span>
+      </Grid>
+      <Grid className={s.text} item xs>
+        <span>{price}</span>
+      </Grid>
+      <Grid item xs>
+        <div className={s.actions}>
+          <div
+            tabIndex={0}
+            role="button"
+            onKeyPress={undefined}
+            onClick={onDelete}
+          >
+            Удалить
+          </div>
+          <div
+            tabIndex={0}
+            role="button"
+            onKeyPress={undefined}
+            onClick={onEdit}
+          >
+            Редактировать
+          </div>
+        </div>
+      </Grid>
+    </Grid>
+  );
+}
