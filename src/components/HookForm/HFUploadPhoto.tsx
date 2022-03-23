efd1b1df9dccfc64d6f8013361b218d97f443e19
MikleBr <barulin.ma@gmail.com>
2022-03-23 17:53:57 +0300

    Сделал UploadImage и HFUploadImage

---
+++ src/components/HookForm/HFUploadPhoto.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { SxProps } from '@mui/material';
+import { Controller, useFormContext } from 'react-hook-form';
+import { UploadImage } from '../UI/UploadImage/UploadImage';
+
+type Props = {
+  name: string;
+  defaultValue?: string;
+  label?: string;
+  type?: string;
+  sx?: SxProps;
+  id: string;
+  allowedFileTypes: ('image/jpeg' | 'image/png' | 'image/webp')[];
+};
+
+export function HFUploadPhoto({ name, defaultValue, ...props }: Props) {
+  const {
+    control,
+    formState: { errors },
+  } = useFormContext();
+
+  return (
+    <Controller
+      name={name}
+      control={control}
+      defaultValue={defaultValue || ''}
+      render={({ field: { ref, ...rest } }) => (
+        <UploadImage
+          {...rest}
+          isError={!!errors[name]}
+          helperText={errors[name]?.message ?? ''}
+          {...props}
+        />
+      )}
+    />
+  );
+}
