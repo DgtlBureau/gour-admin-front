8777532dd0cb1d146edf62908d43cce412ee07b9
zolkinka <32388803+zolkinka@users.noreply.github.com>
2022-04-22 14:57:41 +0400

    добавил компоненты для реферальных кодов

---
+++ src/components/ReferralCodes/ReferralCodeDiscountBlock/ReferralCodeDiscountBlock.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import s from './ReferralCodeDiscountBlock.module.scss';
+
+export type ReferralCodeDiscountBlockProps = {
+    discount: number;
+    onChange(discount: number): void;
+};
+
+export function ReferralCodeDiscountBlock(props: ReferralCodeDiscountBlockProps) {
+    return <div>ReferralCodeDiscountBlock</div>
+}
---
+++ src/components/ReferralCodes/ReferralCodeTable/ReferralCodeTable.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import s from './ReferralCodeTable.module.scss';
+
+export type ReferralCodeTableProps = {
+    codes: {
+        id: number;
+        code: string;
+    }[]
+    onRemove(id: number): void;
+};
+
+export function ReferralCodeTable(props: ReferralCodeTableProps) {
+    return <div>ReferralCodeTable</div>
+}
