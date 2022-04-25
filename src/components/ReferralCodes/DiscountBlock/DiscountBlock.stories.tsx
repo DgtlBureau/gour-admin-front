4ac52da1e7b1dbadba90d4e9dac0661d8e3175d6
Artyom Vorokhta <a.vorohta@newshift.ru>
2022-04-25 16:54:45 +0300

    discount block

---
+++ src/components/ReferralCodes/DiscountBlock/DiscountBlock.stories.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import { ComponentStory, Meta } from '@storybook/react';
+
+import { ReferralCodeDiscountBlock } from './DiscountBlock';
+
+export default {
+  component: ReferralCodeDiscountBlock,
+  title: 'ReferralCodeDiscountBlock',
+} as Meta;
+
+const Template: ComponentStory<typeof ReferralCodeDiscountBlock> = function () {
+  const [discount, setDiscount] = useState(0);
+
+  return <ReferralCodeDiscountBlock discount={discount} onChange={newDiscount => setDiscount(newDiscount)} />;
+};
+export const DefaultReferralCodeDiscountBlock = Template.bind({});
