f63b0487ea777be36de89bd32487fdd6e4ea4212
zolkinka <32388803+zolkinka@users.noreply.github.com>
2022-05-05 20:59:14 +0400

    поправил логику создания пользователя

--- src/view/Users/Create.tsx
+++ src/view/Users/Create.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import { InputLabel } from '@mui/material';
 import { Header } from '../../components/Header/Header';
 import { Button } from '../../components/UI/Button/Button';
 import { Path } from '../../constants/routes';
 import { useTo } from '../../hooks/useTo';
+import { RadioButton } from '../../components/UI/RadioButton/RadioButton';
+import { TextField } from '../../components/UI/TextField/TextField';
+import { useSignupWithoutPasswordMutation } from '../../api/authApi';
 
 type Props = {
   onSaveClick: () => void;
@@ -25,12 +29,18 @@ function RightContent({ onSaveClick, onCancelHandler }: Props) {
 
 function CreateUserView() {
   const to = useTo();
+  const [signupWithoutPasswordMutation] = useSignupWithoutPasswordMutation();
+
+  const [role, setRole] = useState<'admin'|'moderator'>('admin');
+  const [email, setEmail] = useState('');
 
   const onCancelHandler = () => {
     to(Path.USERS);
   };
   const onSaveClick = () => {
-    console.log('Save');
+    signupWithoutPasswordMutation({
+      role, email,
+    });
   };
 
   return (
@@ -41,6 +51,28 @@ function CreateUserView() {
           <RightContent onSaveClick={onSaveClick} onCancelHandler={onCancelHandler} />
         }
       />
+      <InputLabel>
+        Роль:
+      </InputLabel>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        {/* eslint-disable-next-line jsx-a11y/label-has-for */}
+        <label style={{ display: 'flex', alignItems: 'center' }}>
+          <RadioButton checked={role === 'admin'} onChange={() => setRole('admin')} />
+          {' '}
+          Администратор
+        </label>
+        {/* eslint-disable-next-line jsx-a11y/label-has-for */}
+        <label style={{ display: 'flex', alignItems: 'center', marginRight: '20px' }}>
+          <RadioButton checked={role === 'moderator'} onChange={() => setRole('moderator')} />
+          {' '}
+          Модератор
+        </label>
+        <TextField
+          label="email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
+      </div>
     </div>
   );
 }
