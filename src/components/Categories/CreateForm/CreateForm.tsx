3558b80e70be0600b9b5fb2f8dd5a9521ab8507f
Mikhail Barulin <mikhail-barulin@mail.ru>
2022-09-19 15:38:31 +0300

    feat: update create category form and modal

--- src/components/Categories/CreateForm/CreateForm.tsx
+++ src/components/Categories/CreateForm/CreateForm.tsx
@@ -2,28 +2,61 @@ import React from 'react';
 import { FormProvider, SubmitHandler, useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 
-import schema from './validation';
-import { TranslatableStringDto } from '../../../@types/dto/translatable-string.dto';
 import { HFTextField } from '../../HookForm/HFTextField';
+import { HFSelect } from '../../HookForm/HFSelect';
+
+import type { CategoryCreateDto } from '../../../@types/dto/category/create.dto';
+import type { Category } from '../../../@types/entities/Category';
+import schema from './validation';
 
 type Props = {
-  defaultValues: TranslatableStringDto;
-  onSave: SubmitHandler<TranslatableStringDto>;
+  defaultValues: CategoryCreateDto;
+  categories: {
+    label: string;
+    value: number;
+  }[];
+  onSave: SubmitHandler<CategoryCreateDto>;
 };
 
-export function CreateCategoryForm({ defaultValues, onSave }: Props) {
-  const values = useForm<TranslatableStringDto>({
+export type CategoryForm = {
+  title: string;
+  parentCategoryId?: Category['id'];
+};
+
+export function CreateCategoryForm({ defaultValues, categories, onSave }: Props) {
+  const values = useForm<CategoryForm>({
     resolver: yupResolver(schema),
-    defaultValues,
+    defaultValues: {
+      title: defaultValues.title.ru,
+      parentCategoryId: defaultValues.parentCategoriesIds?.[0] || -1,
+    },
   });
 
-  const submit = (data: TranslatableStringDto) => onSave(data);
+  const handleSave = (data: CategoryForm) => {
+    const parentCategoriesIds = data.parentCategoryId
+      ? [data.parentCategoryId]
+      : undefined;
+    onSave({
+      title: {
+        ru: data.title,
+        en: '',
+      },
+      parentCategoriesIds,
+    });
+  };
+
+  const categoriesOptions = [{ label: 'Нет', value: -1 }, ...categories];
 
   return (
     <FormProvider {...values}>
-      <form id="createCategoryForm" onSubmit={values.handleSubmit(submit)}>
-        <HFTextField sx={{ margin: '10px 0' }} label="Название (Рус)" name="ru" />
-        <HFTextField label="Название (Eng)" name="en" />
+      <form id="createCategoryForm" onSubmit={values.handleSubmit(handleSave)}>
+        <HFTextField sx={{ margin: '10px 0' }} label="Название (Рус)" name="title" />
+        <HFSelect
+          name="parentCategoryId"
+          label="Родительская категория"
+          placeholder="Родительская категория"
+          options={categoriesOptions}
+        />
       </form>
     </FormProvider>
   );
