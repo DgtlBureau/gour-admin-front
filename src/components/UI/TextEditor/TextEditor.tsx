6647d99f0190981673253b305b521efa98b0b3cd
Mikhail Barulin <mikhail-barulin@mail.ru>
2022-10-12 08:54:17 +0300

    feat: 🎸 Компонент TextEditor и react-quill

---
+++ src/components/UI/TextEditor/TextEditor.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import ReactQuill from 'react-quill';
+
+type Props = {
+  id?: string;
+  value?: string;
+  label?: string;
+  isError?: boolean;
+  helperText?: boolean;
+  onChange?: (value: string) => void;
+};
+
+export function TextEditor({ id, value, label, isError, helperText, onChange }: Props) {
+  return (
+    <>
+      {label}
+      <ReactQuill id={id} value={value} onChange={onChange} />
+      {helperText}
+    </>
+  );
+}
