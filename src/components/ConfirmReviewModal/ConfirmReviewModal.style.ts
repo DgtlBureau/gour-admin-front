51990d47dba315e15e98792d0998407c603e8747
Mikhail Barulin <mikhail-barulin@mail.ru>
2022-10-13 14:40:58 +0300

    fix: 🐛 Вынес стили. Добавил заголовок и кнопку закрытия

---
+++ src/components/ConfirmReviewModal/ConfirmReviewModal.style.ts
@@ -0,0 +1,18 @@
+export const sx = {
+  modal: {
+    maxWidth: '690px',
+    padding: '25px',
+  },
+  header: {
+    margin: '0 0 10px 0',
+    width: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  buttons: {
+    margin: '25px 0 0 0',
+    display: 'flex',
+    columnGap: '10px',
+  },
+};
