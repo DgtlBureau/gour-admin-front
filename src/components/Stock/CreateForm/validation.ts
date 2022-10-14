b7f0591e62b97401a1d6369d829822aab744b9ca
Mikhail Barulin <74414622+MikleBr@users.noreply.github.com>
2022-10-14 18:09:52 +0300

    fix: 🐛 добавил валидицию даты завершения акции (#47ze3bk)

--- src/components/Stock/CreateForm/validation.ts
+++ src/components/Stock/CreateForm/validation.ts
@@ -3,7 +3,11 @@ import * as yup from 'yup';
 export default yup.object().shape({
   title: yup.string().required('Введите заголовок'),
   start: yup.date().typeError('Некорректная дата').required('Введите дату'),
-  end: yup.date().typeError('Некорректная дата').required('Введите дату'),
+  end: yup
+    .date()
+    .typeError('Некорректная дата')
+    .min(yup.ref('start'), 'Дата завершения должна быть позже даты начала')
+    .required('Введите дату'),
   discount: yup
     .number()
     .typeError('Введите число')
