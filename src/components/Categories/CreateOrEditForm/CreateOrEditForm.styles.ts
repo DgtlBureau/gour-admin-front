9cb1282d9f86b6166889a5f05b648e3f6419ec99
Sergey Smolkov <damager4425@gmail.com>
2022-10-26 16:39:42 +0300

    fix: 🐛 функция-хелпер для создания стилей

--- src/components/Categories/CreateOrEditForm/CreateOrEditForm.styles.ts
+++ src/components/Categories/CreateOrEditForm/CreateOrEditForm.styles.ts
@@ -1,9 +1,9 @@
-import type { SxList } from 'types';
+import { createSx } from 'themes';
 
-export const sx: SxList = {
+export const sx = createSx({
   form: {
     display: 'flex',
     flexDirection: 'column',
     rowGap: '10px',
   },
-};
+});
--- src/components/Users/Table/Table.styles.ts
+++ src/components/Users/Table/Table.styles.ts
@@ -1,12 +1,12 @@
-import { SxList } from 'types';
+import { createSx } from 'themes';
 
-const sx: SxList = {
+const sx = createSx({
   role: {
     width: 'fit-content',
     padding: '6px',
     background: '#F4E7CE',
     borderRadius: '6px',
   },
-};
+});
 
 export default sx;
